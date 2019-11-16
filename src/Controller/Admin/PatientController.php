9b5ab078f7513d7ec7ee7ebf23bc48f4e9bf3a28
CyrilCheru <cy.cheru@gmail.com>
2019-11-16 20:46:55 +0100

    Mise en place des fichiers index, create, edit, show des patients. Des modifications restent encore a faire

--- src/Controller/Admin/PatientController.php
+++ src/Controller/Admin/PatientController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Patient;
+use App\Form\PatientType;
 use App\Repository\PatientRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PatientController extends AbstractController
@@ -20,6 +24,94 @@ class PatientController extends AbstractController
 
         return $this->render('admin/patient/index.html.twig', [
             'patients' => $patients
+        ]);
+    }
+
+    /**
+     * @Route("/admin/patient/{id}/edit", name="admin_patient_edit", requirements={"id": "\d+"})
+     *
+     * @return void
+     */
+    public function edit(Patient $patient, Request $request, EntityManagerInterface $em)
+    {
+
+        // dd($patient);
+        $form = $this->createForm(PatientType::class, $patient);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                "Le Patient a bien été modifié dans la base de données"
+            );
+
+            return $this->redirectToRoute("admin_patient_show", [
+                "id" => $patient->getId()
+            ]);
+        }
+
+        return $this->render('admin/patient/edit.html.twig', [
+            'form' => $form->createView(),
+            'patient' => $patient
+        ]);
+    }
+
+    /**
+     * @Route("/admin/patient/nouveau", name="admin_patient_create")
+     *
+     * @return void
+     */
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepository)
+    {
+
+        $nurses = $userRepository->findBy([
+            'job' => 2
+        ]);
+
+        $doctors = $userRepository->findBy([
+            'job' => 1
+        ]);
+
+        $patient = new Patient;
+
+        $form = $this->createForm(PatientType::class, $patient);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($patient);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                "Le nouveau patient, <strong>{$patient->getLastName()} {$patient->getFirstName()} a bien été enregistré !"
+            );
+
+            return $this->redirectToRoute("admin_patient_show", [
+                'id' => $patient->getId()
+            ]);
+        }
+
+        return $this->render('admin/patient/create.html.twig', [
+            'form' => $form->createView(),
+            'nurses' => $nurses,
+            'doctors' => $doctors
+        ]);
+    }
+
+    /**
+     * @Route("/admin/patient/{id}", name="admin_patient_show")
+     *
+     * @return void
+     */
+    public function show(Patient $patient)
+    {
+
+        return $this->render("admin/patient/show.html.twig", [
+            'patient' => $patient
         ]);
     }
 }
--- src/Form/PatientType.php
+++ src/Form/PatientType.php
@@ -3,7 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Patient;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +19,103 @@ class PatientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('gender')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthDate')
-            ->add('address1')
-            ->add('address2')
-            ->add('zipCode')
-            ->add('city')
-            ->add('phone')
-            ->add('email')
-            ->add('doctor')
-            ->add('nurses')
-        ;
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    '-- Choississez --' => null,
+                    'Femme' => "Femme",
+                    'Homme' => "Homme"
+                ],
+                'label' => "Sexe",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => "Nom",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => "Prénom",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('birthDate', DateType::class, [
+                'label' => "Date de naissance",
+                'format' => "dd-MM-yyyy",
+                'years' => range(1900, 2020),
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('address1', TextType::class, [
+                'label' => "Adresse",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('address2', TextType::class, [
+                'label' => "Complément d'addresse",
+                'required' => false,
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('zipCode', TextType::class, [
+                'label' => "Code Postal",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => "Ville",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => "Téléphone",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => "Email",
+                'required' => false,
+                'attr' => [
+                    'class' => "form-control",
+                    'placeholder' => "Si le patient en a une"
+                ]
+            ])
+            ->add('doctor', EntityType::class, [
+                'class' => User::class,
+                'group_by' => "job.title",
+                'query_builder' => function (UserRepository $user) {
+                    return $user->createQueryBuilder('u')
+                        ->orderBy('u.job', 'ASC');
+                },
+                'choice_label' => 'lastName',
+                'label' => "Médecin traitant",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+
+            ])
+            ->add('nurses', EntityType::class, [
+                'class' => User::class,
+                'group_by' => "job.title",
+                'query_builder' => function (UserRepository $user) {
+                    return $user->createQueryBuilder('u')
+                        ->orderBy('u.job', 'ASC');
+                },
+                'choice_label' => 'lastName',
+                'label' => "Infirmier(e)s",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
